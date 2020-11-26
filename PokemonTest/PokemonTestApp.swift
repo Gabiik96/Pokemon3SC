0f2d88fa22b03d503d3ff7fccc6eca73b225a62b
Gabriel Balta <g.balta96@gmail.com>
2020-11-26 23:15:11 +0000

    init commit

---
+++ PokemonTest/PokemonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTestApp.swift
+//  PokemonTest
+//
+//  Created by Gabriel Balta on 26/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
