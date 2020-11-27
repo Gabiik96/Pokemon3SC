feb1f7c8b08719ae1fc1e9707496c4e78b000e86
Gabriel Balta <g.balta96@gmail.com>
2020-11-27 20:29:43 +0000

    Update: Networking implementation for LoadingView -> now gets amount of all pokemons before fetching & progressValue added for progressBar

--- PokemonTest/Services/NetworkingCustom.swift
+++ PokemonTest/Services/NetworkingCustom.swift
@@ -11,19 +11,42 @@ import PokemonAPI
 class APICustom: ObservableObject {
     @Published var pokemonStore = [PKMPokemon]()
     
-    var limit: Int = 20
-
-    func getPokemons(limit: Int) {
-        for id in 0 ..< limit {
-            PokemonAPI().pokemonService.fetchPokemon(id){ result in
+    @Published var progressValue: Float = 0
+    @Published var isLoading = true
+    let pokeAPI = PokemonAPI()
+    
+    
+    var limit = 0
+    
+    func getLimit(){
+        self.pokeAPI.pokemonService.fetchPokemonList(){ result in
             switch result {
-            case .success(let pokemon):
-                self.pokemonStore.append(pokemon)
+            case .success(let result):
+                
+                self.limit = result.count!
+                self.getPokemons()
                 
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func getPokemons() {
+        for id in 0 ..< limit {
+            pokeAPI.pokemonService.fetchPokemon(id){ result in
+                switch result {
+                case .success(let pokemon):
+                    self.pokemonStore.append(pokemon)
+                    self.progressValue += (1 / Float(self.limit))
+                    print(self.progressValue)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.progressValue += (1 / Float(self.limit))
+                    print(self.progressValue)
+                }
+            }
         }
     }
+    
 }
