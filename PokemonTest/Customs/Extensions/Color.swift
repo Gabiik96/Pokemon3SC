7e7f4f27f25ccf0e8f2e57cb32fdbd9ca5561a92
Gabriel Balta <g.balta96@gmail.com>
2020-11-27 21:39:46 +0000

    Build: Pokemon colour set -> assets + extension & pokemonLogo asset

---
+++ PokemonTest/Customs/Extensions/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  PokemonTest
+//
+//  Created by Gabriel Balta on 27/11/2020.
+//
+
+import SwiftUI
+
+extension Color {
+    public static var pokemonBlue: Color {
+        Color("pokemonBlue", bundle: nil)
+    }
+    
+    public static var pokemonYellow: Color {
+        Color("pokemonYellow", bundle: nil)
+    }
+}
