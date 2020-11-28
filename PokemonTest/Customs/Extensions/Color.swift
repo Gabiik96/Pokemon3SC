7b641fb06230fac75f2d769cad1e4941f1433740
Gabriel Balta <g.balta96@gmail.com>
2020-11-28 00:03:18 +0000

    Update: PKMPokemon property added "backgroundColor" -> depends on pokemon type & Color extension updated to work with HEX

--- PokemonTest/Customs/Extensions/Color.swift
+++ PokemonTest/Customs/Extensions/Color.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 extension Color {
+    
+    init(_ hex: UInt32, opacity:Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+    
     public static var pokemonBlue: Color {
         Color("pokemonBlue", bundle: nil)
     }
@@ -15,4 +23,5 @@ extension Color {
     public static var pokemonYellow: Color {
         Color("pokemonYellow", bundle: nil)
     }
+    
 }
---
+++ PokemonTest/Customs/Extensions/PKMPokemon.swift
@@ -0,0 +1,36 @@
+//
+//  PKMPokemon.swift
+//  PokemonTest
+//
+//  Created by Gabriel Balta on 27/11/2020.
+//
+
+import SwiftUI
+import PokemonAPI
+
+extension PKMPokemon {
+    var backgroundColor: Color {
+        switch types?.first?.type!.name {
+            case "normal":  return Color(0xA8A77A)
+            case "fighting":  return Color(0xC22E28)
+            case "flying":  return Color(0xA98FF3)
+            case "poison":  return Color(0xA33EA1)
+            case "ground":  return Color(0xE2BF65)
+            case "rock":  return Color(0xB6A136)
+            case "bug":  return Color(0xA6B91A)
+            case "ghost":  return Color(0x735797)
+            case "fire":  return Color(0xEE8130)
+            case "water":  return Color(0x6390F0)
+            case "grass":  return Color(0x7AC74C)
+            case "electric":  return Color(0xF7D02C)
+            case "psychic":  return Color(0xF95587)
+            case "ice":  return Color(0x96D9D6)
+            case "dragon":  return Color(0x6F35FC)
+            case "dark":  return Color(0x705746)
+            case "fairy":  return Color(0xD685AD)
+            case "steel":  return Color(0xB7B7CE)
+            default :  return Color.black
+        }
+    }
+}
+
