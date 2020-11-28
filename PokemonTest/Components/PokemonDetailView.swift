22de39fbb8063b9cb64e8ff124b2d70da4e089fc
Gabriel Balta <g.balta96@gmail.com>
2020-11-28 11:08:35 +0000

    Update: Refactored PokemonDetailView

--- PokemonTest/Components/PokemonDetailView.swift
+++ PokemonTest/Components/PokemonDetailView.swift
@@ -15,9 +15,8 @@ struct PokemonDetailView: View {
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(pokemon.backgroundColor)
-                .edgesIgnoringSafeArea(.all)
+            background
+            
             VStack {
                 ZStack(alignment: .top) {
                     RoundedRectangle(cornerRadius: 100)
@@ -25,43 +24,57 @@ struct PokemonDetailView: View {
                         .opacity(0.5)
                         .padding()
                     ZStack {
-                        VStack(alignment: .center) {
-                            ImageView(withURL: (self.isShiny ? self.pokemon.sprites?.frontShiny : self.pokemon.sprites?.frontDefault)!)
-                                    .frame(width: 400, height: 180)
-                            ImageView(withURL: (self.isShiny ? self.pokemon.sprites?.backShiny : self.pokemon.sprites?.backDefault)!)
-                                .frame(width: 400, height: 180)
-                        }
+                        pokemonSprites
                         HStack {
                             Spacer()
-                            VStack(alignment: .trailing) {
-                                Spacer()
-                                Button(action: {
-                                    isShiny.toggle()
-                                }){
-                                    Image(systemName: self.isShiny ? "sun.max.fill" : "sun.max")
-                                        .resizable()
-                                        .shadow(color: .pokemonBlue, radius: 1.5)
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(self.isShiny ? .pokemonYellow : .pokemonBlue)
-                                        .frame(width: 50, height: 50)
-                                }
-                                Spacer()
-                                Spacer()
-                            }.padding(.trailing, 25)
+                                shineBtn
                         }
                     }
                     
                 }.frame(height: 300)
                 
-                StrokeText(text: pokemon.name!.uppercased(), width: 4, color: .pokemonBlue)
-                    .foregroundColor(.yellow)
-                    .padding(.top)
-                    .font(.PokemonSolid(size: 50))
+                pokemonName
                 
                 Spacer ()
                 
             }
         }
         
+    }
+    
+    private var background: some View {
+        Rectangle()
+            .fill(pokemon.backgroundColor)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    private var pokemonSprites: some View {
+        VStack(alignment: .center) {
+            ImageView(withURL: (self.isShiny ? self.pokemon.sprites?.frontShiny : self.pokemon.sprites?.frontDefault)!)
+                    .frame(width: 400, height: 180)
+            ImageView(withURL: (self.isShiny ? self.pokemon.sprites?.backShiny : self.pokemon.sprites?.backDefault)!)
+                .frame(width: 400, height: 180)
+        }
+    }
+    
+    private var shineBtn: some View {
+        Button(action: {
+            isShiny.toggle()
+        }){
+            Image(systemName: self.isShiny ? "sun.max.fill" : "sun.max")
+                .resizable()
+                .shadow(color: .pokemonBlue, radius: 1.5)
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(self.isShiny ? .pokemonYellow : .pokemonBlue)
+                .frame(width: 50, height: 50)
+                .padding(.trailing, 25)
+        }
+    }
+    
+    private var pokemonName: some View {
+        StrokeText(text: pokemon.name!.uppercased(), width: 4, color: .pokemonBlue)
+            .foregroundColor(.yellow)
+            .padding(.top)
+            .font(.PokemonSolid(size: 50))
     }
 }
