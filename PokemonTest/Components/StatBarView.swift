ea8a22658fbd4e12dfa7b51bc333869f107089e0
Gabriel Balta <g.balta96@gmail.com>
2020-11-28 20:40:42 +0000

    Build: StatView UI with data calculation for each pokemon stat

--- PokemonTest/Components/StatBarView.swift
+++ PokemonTest/Components/StatBarView.swift
@@ -5,4 +5,45 @@
 //  Created by Gabriel Balta on 28/11/2020.
 //
 
-import Foundation
+import SwiftUI
+import PokemonAPI
+
+struct StatBarView: View {
+    
+    var color: Color = Color.black
+    var height: CGFloat = 255
+    var width: CGFloat = 15
+    var fill: CGFloat = 0
+    var statName: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text(statName)
+                .fontWeight(.black)
+                .shadow(radius: 1)
+                .foregroundColor(color)
+                
+
+            ZStack(alignment: .bottom) {
+                Rectangle()
+                    .fill(Color.white)
+                    .opacity(0.5)
+                    .frame(width: (width * (UIScreen.main.bounds.size.width / 250)),
+                           height: (255 * (UIScreen.main.bounds.size.height / 1300)))
+                Rectangle()
+                    .fill(color)
+                    .frame(width: (width * (UIScreen.main.bounds.size.width / 250)), height: (((height / 100) * fill) * (UIScreen.main.bounds.size.height / 1300)))
+            }.padding(.bottom, 5)
+        }
+        .padding(.leading, 5)
+        .padding(.trailing, 5)
+        .background(Rectangle()
+                        .fill(Color.clear)
+                        .opacity(0.5)
+                        .overlay(RoundedRectangle(cornerRadius: 2)
+                                    .fill(Color.black)
+                                    .opacity(0.3)
+                                    .frame(width: (width * (UIScreen.main.bounds.size.width / 190)))))
+    }
+    
+}
--- PokemonTest/Components/StatsView.swift
+++ PokemonTest/Components/StatsView.swift
@@ -5,4 +5,38 @@
 //  Created by Gabriel Balta on 28/11/2020.
 //
 
-import Foundation
+import SwiftUI
+import PokemonAPI
+
+struct StatsView: View {
+    
+    let statsResource: [PKMPokemonStat]
+    
+    var body: some View {
+            VStack(alignment: .leading) {
+                Spacer()
+                HStack {
+                    if let hpStat = statsResource.first(where: { $0.stat!.name!.contains("hp") }) {
+                        StatBarView(color: Color.red, width: 20, fill: CGFloat(hpStat.baseStat!), statName: "HP")
+                    }
+                    if let attackStat = statsResource.first(where: { $0.stat!.name!.contains("attack") }) {
+                        StatBarView(color: Color.orange, height: 170, width: 20, fill: CGFloat(attackStat.baseStat!), statName: "ATT")
+                    }
+                    if let defenseStat = statsResource.first(where: { $0.stat!.name!.contains("defense") }) {
+                        StatBarView(color: Color.yellow, height: 230, width: 20, fill: CGFloat(defenseStat.baseStat!), statName: "DEF")
+                    }
+                    if let specialATTStat = statsResource.first(where: { $0.stat!.name!.contains("special-attack") }) {
+                        StatBarView(color: Color.blue, height: 154, width: 20, fill: CGFloat(specialATTStat.baseStat!), statName: "*ATT")
+                    }
+                    if let specialDEFStat = statsResource.first(where: { $0.stat!.name!.contains("special-defense") }) {
+                        StatBarView(color: Color.green, height: 230, width: 20, fill: CGFloat(specialDEFStat.baseStat!), statName: "*DEF")
+                    }
+                    if let speedStat = statsResource.first(where: { $0.stat!.name!.contains("speed") }) {
+                        StatBarView(color: Color.pink, height: 160, width: 20, fill: CGFloat(speedStat.baseStat!), statName: "SPE")
+                    }
+            }
+            Spacer()
+        }
+    }
+    
+}
